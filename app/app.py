7bdb62fad633d9aa649af5a6a946708292a8787a
ryanmelink <rsm73@njit.edu>
2020-07-29 17:57:32 -0400

    Task: removed cities

--- app/app.py
+++ app/app.py
@@ -12,53 +12,53 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'citiesData'
+app.config['MYSQL_DATABASE_DB'] = 'gradesData'
 mysql.init_app(app)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    user = {'username': 'Cities Project'}
+    user = {'username': 'Grades Project'}
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM fldGrade')
     result = cursor.fetchall()
-    return render_template('index.html', title='Home', user=user, cities=result)
+    return render_template('index.html', title='Home', user=user, grades=result)
 
 
-@app.route('/view/<int:city_id>', methods=['GET'])
-def record_view(city_id):
+@app.route('/view/<int:grade_id>', methods=['GET'])
+def record_view(grade_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM fldGrade WHERE id=%s', city_id)
+    cursor.execute('SELECT * FROM fldGrade WHERE id=%s', grade_id)
     result = cursor.fetchall()
-    return render_template('view.html', title='View Form', city=result[0])
+    return render_template('view.html', title='View Form', grade=result[0])
 
 
-@app.route('/edit/<int:city_id>', methods=['GET'])
-def form_edit_get(city_id):
+@app.route('/edit/<int:grade_id>', methods=['GET'])
+def form_edit_get(grade_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM fldGrade WHERE id=%s', city_id)
+    cursor.execute('SELECT * FROM fldGrade WHERE id=%s', grade_id)
     result = cursor.fetchall()
-    return render_template('edit.html', title='Edit Form', city=result[0])
+    return render_template('edit.html', title='Edit Form', grade=result[0])
 
 
-@app.route('/edit/<int:city_id>', methods=['POST'])
-def form_update_post(city_id):
+@app.route('/edit/<int:grade_id>', methods=['POST'])
+def form_update_post(grade_id):
     cursor = mysql.get_db().cursor()
     inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
                  request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
-                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'), city_id)
+                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'), grade_id)
     sql_update_query = """UPDATE fldGrade t SET t.fldLastName = %s, t.fldFirstName = %s, t.fldSSN = %s, t.fldTest1 = 
     %s, t.fldTest2 = %s, t.fldTest3 = %s, t.fldTest4 = %s WHERE t.id = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
-@app.route('/cities/new', methods=['GET'])
+@app.route('/grades/new', methods=['GET'])
 def form_insert_get():
-    return render_template('new.html', title='New City Form')
+    return render_template('new.html', title='New Grade Form')
 
 
-@app.route('/cities/new', methods=['POST'])
+@app.route('/grades/new', methods=['POST'])
 def form_insert_post():
     cursor = mysql.get_db().cursor()
     inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
@@ -69,16 +69,16 @@ def form_insert_post():
     mysql.get_db().commit()
     return redirect("/", code=302)
 
-@app.route('/delete/<int:city_id>', methods=['POST'])
-def form_delete_post(city_id):
+@app.route('/delete/<int:grade_id>', methods=['POST'])
+def form_delete_post(grade_id):
     cursor = mysql.get_db().cursor()
     sql_delete_query = """DELETE FROM fldGrade WHERE id = %s """
-    cursor.execute(sql_delete_query, city_id)
+    cursor.execute(sql_delete_query, grade_id)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/api/v1/cities', methods=['GET'])
+@app.route('/api/v1/grades', methods=['GET'])
 def api_browse() -> str:
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM fldGrade')
@@ -88,23 +88,23 @@ def api_browse() -> str:
     return resp
 
 
-@app.route('/api/v1/cities/<int:city_id>', methods=['GET'])
-def api_retrieve(city_id) -> str:
+@app.route('/api/v1/grades/<int:grade_id>', methods=['GET'])
+def api_retrieve(grade_id) -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM fldGrade WHERE id=%s', city_id)
+    cursor.execute('SELECT * FROM fldGrade WHERE id=%s', grade_id)
     result = cursor.fetchall()
     json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/cities/<int:city_id>', methods=['PUT'])
-def api_edit(city_id) -> str:
+@app.route('/api/v1/grades/<int:grade_id>', methods=['PUT'])
+def api_edit(grade_id) -> str:
     cursor = mysql.get_db().cursor()
     content = request.json
     inputData = (content['fldName'], content['fldLat'], content['fldLong'],
                  content['fldCountry'], content['fldAbbreviation'],
-                 content['fldCapitalStatus'], content['fldPopulation'],city_id)
+                 content['fldCapitalStatus'], content['fldPopulation'],grade_id)
     sql_update_query = """UPDATE fldGrade t SET t.fldLastName = %s, t.fldFirstName = %s, t.fldSSN = %s, t.fldTest1 = 
         %s, t.fldTest2 = %s, t.fldTest3 = %s, t.fldTest4 = %s WHERE t.id = %s """
     cursor.execute(sql_update_query, inputData)
@@ -112,7 +112,7 @@ def api_edit(city_id) -> str:
     resp = Response(status=200, mimetype='application/json')
     return resp
 
-@app.route('/api/v1/cities', methods=['POST'])
+@app.route('/api/v1/grades', methods=['POST'])
 def api_add() -> str:
 
     content = request.json
@@ -127,11 +127,11 @@ def api_add() -> str:
     resp = Response(status=201, mimetype='application/json')
     return resp
 
-@app.route('/api/v1/cities/<int:city_id>', methods=['DELETE'])
-def api_delete(city_id) -> str:
+@app.route('/api/v1/grades/<int:grade_id>', methods=['DELETE'])
+def api_delete(grade_id) -> str:
     cursor = mysql.get_db().cursor()
     sql_delete_query = """DELETE FROM fldGrade WHERE id = %s """
-    cursor.execute(sql_delete_query, city_id)
+    cursor.execute(sql_delete_query, grade_id)
     mysql.get_db().commit()
     resp = Response(status=200, mimetype='application/json')
     return resp
